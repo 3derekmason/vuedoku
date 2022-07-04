d0b8576d6f47b794ad4bd2ae72ffd4edd6a3d814
Derek Mason <3derekmason@gmail.com>
2022-07-04 10:17:17 -0600

    validate board passing initial tests

--- src/components/__tests__/validateArray.spec.js
+++ src/components/__tests__/validateArray.spec.js
@@ -3,7 +3,7 @@ import { expect, it } from "vitest";
 import validateArray from "../util/validateArray";
 
 const valid = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const repeats = [1, 2, 3, 4, 5, 3, 6, 9, 8];
+const repeats = [3, 9, 1, 2, 8, 9, 5, 7, 4];
 const invalidRange = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 it("returns true if array contains no duplicates", () => {
--- src/components/util/validateGameBoard.js
+++ src/components/util/validateGameBoard.js
@@ -1,5 +1,32 @@
 import validateArray from "./validateArray";
+import checkBoardSize from "./checkBoardSize";
+import defineColumns from "./defineColumns";
+import defineBoxes from "./defineBoxes";
 
-const validateGameBoard = (boardArray) => {};
+const validateGameBoard = (boardArray) => {
+  let isValid = checkBoardSize(boardArray);
+  const columns = defineColumns(boardArray);
+  const boxes = defineBoxes(boardArray);
+
+  boardArray.forEach((row) => {
+    if (!validateArray(row)) {
+      isValid = false;
+    }
+  });
+
+  columns.forEach((column) => {
+    if (!validateArray(column)) {
+      isValid = false;
+    }
+  });
+
+  boxes.forEach((box) => {
+    if (!validateArray(box)) {
+      isValid = false;
+    }
+  });
+
+  return isValid;
+};
 
 export default validateGameBoard;
