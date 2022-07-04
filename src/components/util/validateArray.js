1a8fbba5ed5d61bdbc544ce12e0f4f36d19ab77b
Derek Mason <3derekmason@gmail.com>
2022-07-04 09:52:15 -0600

    validate array 1.0

--- src/components/util/validateArray.js
+++ src/components/util/validateArray.js
@@ -1,3 +1,13 @@
-const validateArray = () => {};
+const validateArray = (array) => {
+  const checkArray = [];
+  let result = true;
+  array.forEach((value) => {
+    if (checkArray.includes(value)) {
+      result = false;
+    }
+    checkArray.push(value);
+  });
+  return result;
+};
 
 export default validateArray;
