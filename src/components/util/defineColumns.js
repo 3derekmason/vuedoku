3fb9ce37fb20eeee8861433903edd19eea8a85fc
Derek Mason <3derekmason@gmail.com>
2022-07-03 19:10:15 -0600

    defineColumns init

--- src/components/util/defineColumns.js
+++ src/components/util/defineColumns.js
@@ -1,3 +1,14 @@
-const defineColumns = () => {};
+const defineColumns = (arrayOfRows) => {
+  let arrayOfColumns = [];
+  arrayOfRows.forEach(() => {
+    arrayOfColumns.push([]);
+  });
+  arrayOfRows.forEach((row) => {
+    row.forEach((value, i) => {
+      arrayOfColumns[i].push(value);
+    });
+  });
+  return arrayOfColumns;
+};
 
 export default defineColumns;
