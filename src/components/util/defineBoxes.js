a8bdca432da19f72dee25946bc6ba8070d5acfed
Derek Mason <3derekmason@gmail.com>
2022-07-04 09:45:35 -0600

    define boxes passing first test

--- src/components/util/defineBoxes.js
+++ src/components/util/defineBoxes.js
@@ -3,6 +3,31 @@ const defineBoxes = (arrayOfRows) => {
   arrayOfRows.forEach(() => {
     arrayOfBoxes.push([]);
   });
+  let boxSet = 0;
+  let box = 0;
+  let count = 1;
+  arrayOfRows.forEach((row, i) => {
+    if (i < 3) {
+      boxSet = 0;
+    }
+    if (i >= 3 && i < 6) {
+      boxSet = 3;
+    }
+    if (i > 5) {
+      boxSet = 6;
+    }
+    row.forEach((value) => {
+      if (box % 3 === 0) {
+        box = boxSet;
+      }
+      arrayOfBoxes[box].push(value);
+      if (count % 3 === 0) {
+        box++;
+      }
+
+      count++;
+    });
+  });
   return arrayOfBoxes;
 };
 
