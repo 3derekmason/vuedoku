8d56b71131af7917fb3b9745f75005c47ef99936
Derek Mason <3derekmason@gmail.com>
2022-07-05 11:19:59 -0600

    PuzzleCell init

--- src/components/PuzzleContainer/Puzzle/PuzzleCell.vue
+++ src/components/PuzzleContainer/Puzzle/PuzzleCell.vue
@@ -1,7 +1,17 @@
 <script></script>
 
 <template>
-  <div></div>
+  <div class="puzzleCell"></div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.puzzleCell {
+  height: 100%;
+  width: calc(100% / 9);
+  border: 0.5px solid #ffffff80;
+}
+
+.puzzleCell:hover {
+  border: 1px solid #ffffff;
+}
+</style>
--- src/components/PuzzleContainer/Puzzle/PuzzleRow.vue
+++ src/components/PuzzleContainer/Puzzle/PuzzleRow.vue
@@ -1,7 +1,26 @@
-<script></script>
+<script>
+import PuzzleCell from "./PuzzleCell.vue";
+
+export default {
+  name: "PuzzleRow",
+  components: {
+    PuzzleCell,
+  },
+};
+</script>
 
 <template>
-  <div class="puzzleRow"></div>
+  <div class="puzzleRow">
+    <PuzzleCell />
+    <PuzzleCell />
+    <PuzzleCell />
+    <PuzzleCell />
+    <PuzzleCell />
+    <PuzzleCell />
+    <PuzzleCell />
+    <PuzzleCell />
+    <PuzzleCell />
+  </div>
 </template>
 
 <style scoped>
@@ -9,5 +28,6 @@
   width: 100%;
   height: calc(100% / 9);
   border: 0.5px solid #ffffff60;
+  display: flex;
 }
 </style>
