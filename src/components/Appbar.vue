44a60ac7968731168e7b914fae7712ea3973ecb3
Derek Mason <3derekmason@gmail.com>
2022-07-05 10:48:08 -0600

    Appbar style init

--- src/components/Appbar.vue
+++ src/components/Appbar.vue
@@ -1,7 +1,32 @@
 <script></script>
 
 <template>
-  <div class="appbar">Appbar</div>
+  <div class="appbar">
+    <h1>Sudoku Vue</h1>
+    <div class="buttonRow">
+      <p>Read about the app</p>
+      <a href="">Logic</a>
+      <a href="">UI</a>
+      <a href="">Logic + UI/UX"</a>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.appbar {
+  padding-left: 40px;
+  padding-right: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 88px;
+}
+
+.appbar .buttonRow {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+}
+</style>
