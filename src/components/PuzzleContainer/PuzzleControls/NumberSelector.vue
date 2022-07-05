677bc6e3b99750a89517c9a544db39e0ac17bd53
Derek Mason <3derekmason@gmail.com>
2022-07-05 11:36:39 -0600

    PuzzleControls, NumberSelector init

--- src/components/PuzzleContainer/PuzzleControls/NumberSelector.vue
+++ src/components/PuzzleContainer/PuzzleControls/NumberSelector.vue
@@ -1,7 +1,39 @@
-<script></script>
+<script>
+export default {
+  name: "PuzzleControls",
+  data() {
+    return {
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    };
+  },
+};
+</script>
 
 <template>
-  <div></div>
+  <div class="numberSelector">
+    <div class="selection" v-for="number in numbers" :key="number">
+      <p>{{ numbers[number - 1] }}</p>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.numberSelector {
+  width: 80%;
+  height: 48px;
+  border: 1px solid white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+}
+.numberSelector .selection {
+  width: calc(80% / 9);
+  display: grid;
+  place-content: center;
+  font-size: 1.5em;
+}
+.numberSelector .selection:hover {
+  font-size: 2em;
+}
+</style>
--- src/components/PuzzleContainer/PuzzleControls/PuzzleControls.vue
+++ src/components/PuzzleContainer/PuzzleControls/PuzzleControls.vue
@@ -1,7 +1,19 @@
-<script></script>
+<script>
+import NumberSelector from "./NumberSelector.vue";
+
+export default {
+  name: "PuzzleControls",
+  components: {
+    NumberSelector,
+  },
+};
+</script>
 
 <template>
-  <div class="puzzleControls"></div>
+  <div class="puzzleControls">
+    <div class="undo"></div>
+    <NumberSelector />
+  </div>
 </template>
 
 <style scoped>
@@ -9,5 +21,15 @@
   width: 100%;
   height: 88px;
   border-top: 0.5px solid #ffffff80;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+}
+
+.puzzleControls .undo {
+  width: 48px;
+  aspect-ratio: 1/1;
+  border: 1px solid white;
 }
 </style>
