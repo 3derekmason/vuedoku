4dc08e40d8c82243aeda602a88d512ab60c5c00e
Derek Mason <3derekmason@gmail.com>
2022-07-15 14:38:50 -0600

    remove bug in board is full

--- src/components/util/boardIsFull.js
+++ src/components/util/boardIsFull.js
@@ -1,7 +1,11 @@
 const boardIsFull = (boardArray) => {
   let result = true;
   boardArray.forEach((row) => {
-    result = !row.includes(0);
+    let count = 0;
+    row.forEach((value) => (count += value));
+    if (count !== 45) {
+      result = false;
+    }
   });
   return result;
 };
